e9e374c5a761082be5157651373d58efa8f4dc04
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-07-22 14:36:57 -0400

    finally everything with Category edit works but THe thing is what that I'm thinking since the id stays the same and in the test the Project are still there for that category so I don't have to worry about manipulation that data, now the last thing before moving on to the main course of action is the delete category inside of edit category which means I need to think and manipulate Project that have that ref to Category a new Category to make an uncategorized category and if others categories are deleted so are those projects that are in different category will move there, this is another day of very intricate work that needs to be done for edge cases

--- routes/categories/CategoryRoutes.js
+++ routes/categories/CategoryRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { newCategoryGET, newCategoryPOST, editCategoryGET, editCategoryPOST, } = require('./controllers/CategoryController');
+const { newCategoryGET, newCategoryPOST, editCategoryGET, editCategoryNoCategorySelectedToEditPUT, editCategoryPUT, } = require('./controllers/CategoryController');
 const { isThereAuth } = require('../utils/isThereAuth');
 
 router.get('/new-category', isThereAuth, newCategoryGET);
 router.post('/new-category', isThereAuth, newCategoryPOST);
 
 router.get('/edit-category', isThereAuth, editCategoryGET);
-router.put('/edit-category/:categoryId', isThereAuth, editCategoryPOST);
+router.put('/edit-category/emptyBLEhDKLSNMCKLVKLSKLEUEENFIMDFHNDSKLJHsshkdfnvklnxcnmlDKJFGSHKDGsldkjlkcvxmxlkjdlfgkslgkhzlxckvbnmklzhgfklhd', isThereAuth, editCategoryNoCategorySelectedToEditPUT);
+router.put('/edit-category/:categoryName', isThereAuth, editCategoryPUT);
 
 module.exports = router
--- routes/categories/controllers/CategoryController.js
+++ routes/categories/controllers/CategoryController.js
@@ -68,46 +68,65 @@ module.exports = {
       res.redirect(301, '/api/users/projects/all-projects');
     }
   },
-  editCategoryPOST: async (req, res) => {
+
+  editCategoryNoCategorySelectedToEditPUT: async (req, res) => {
     try {
-      const { categoryName, categoryColor } = req.body;
+      req.flash('messages', 'You have not selected Category to Edit please choose category from dropdown');
+      res.redirect(301, '/api/users/projects/categories/edit-category');
+    } catch (error) {
+    req.flash('errors', 'We cannot get to New Category page at the moment please try again and contact developer');
+    res.redirect(301, '/api/users/projects/all-projects');
+    }
+  },
+
+  editCategoryPUT: async (req, res) => {
+    try {
+      const { toChangeCategoryNewName, categoryNameDropDown, toChangeCategoryNewColor } = req.body;
+      const paramsCategoryName = req.params.categoryName;
   
-      if(!categoryName || !categoryColor) {
-        req.flash('errors', 'Please provide category name and category color these fields are required');
-        return res.redirect(301, '/api/users/projects/categories/new-category');
+      if(!toChangeCategoryNewName || !categoryNameDropDown || !toChangeCategoryNewColor) {
+        req.flash('errors', 'Please provide category name and color with chosen category to change these fields are required otherwise if don\'t want to change anything go back to All Projects');
+        return res.redirect(301, '/api/users/projects/categories/edit-category');
       }
       
       let currUser = await User.findOne({ _id: req.user._id});
       if (currUser) {
-        let allCategoriesFindAndCompareName = await Category.findOne({ categoryName: categoryName, owner: currUser._id }).catch((error) => console.log(error));
-        let allCategoriesFindAndCompareColor = await Category.findOne({ categoryColor: categoryColor, owner: currUser._id }).catch((error) => console.log(error));
-        
+        let allCategoriesFindAndCompareName = await Category.findOne({ categoryName: toChangeCategoryNewName, owner: currUser._id }).catch((error) => console.log(error));
+        let allCategoriesFindAndCompareColor = await Category.findOne({ categoryColor: toChangeCategoryNewColor, owner: currUser._id }).catch((error) => console.log(error));
         if(allCategoriesFindAndCompareName) {
           req.flash('errors', 'category name already exists please type another unique name');
-          return res.redirect(301, '/api/users/projects/categories/new-category');
+          return res.redirect(301, '/api/users/projects/categories/edit-category');
         } else if (allCategoriesFindAndCompareColor) {
           req.flash('errors', 'category color already exists please type another unique color');
-          return res.redirect(301, '/api/users/projects/categories/new-category');
+          return res.redirect(301, '/api/users/projects/categories/edit-category');
         } else if (allCategoriesFindAndCompareName && allCategoriesFindAndCompareColor) {
           req.flash('errors', 'Please provide category name and category color combination that is unique from others that already exist');
-          return res.redirect(301, '/api/users/projects/categories/new-category');
+          return res.redirect(301, '/api/users/projects/categories/edit-category');
         }
-        let newCategory = await new Category();
-        if(categoryName) newCategory.categoryName = categoryName;
-        if(categoryColor) newCategory.categoryColor = categoryColor;
-        
-        newCategory.owner = currUser._id;
-        await newCategory.save().then((savedCategory) => {
-          req.flash('messages', 'You have successfully created a new category, try creating new Project for it from create project tab and choosing it from dropdown menu');
-          res.redirect(301, '/api/users/projects/all-projects');
+        await Category.findOne({ categoryName: categoryNameDropDown, owner: currUser._id})
+        .then((foundCategory) => {
+
+          if(toChangeCategoryNewName) foundCategory.categoryName = toChangeCategoryNewName;
+  
+          if(toChangeCategoryNewColor) foundCategory.categoryColor = toChangeCategoryNewColor;
+          foundCategory.save().then((savedCategory) => {
+            req.flash('messages', 'You have successfully created a new category, try creating new Project for it from create project tab and choosing it from dropdown menu');
+            res.redirect(301, '/api/users/projects/all-projects');
+
+          }).catch((error) => {
+            req.flash('errors', 'We cannot get to Edit Category page at the moment please try again and contact developer');
+            res.redirect(301, '/api/users/projects/all-projects');
+          });
         })
+        
         .catch((error) => {
-          req.flash('errors', 'We couldn\'t create your Category Something is wrong on our end please contact developer or try again');
-          res.redirect(301, '/api/users/projects/create-project');
+          req.flash('errors', 'We cannot get to Edit Category page at the moment please try again and contact developer');
+          res.redirect(301, '/api/users/projects/all-projects');
         });
+
       }
     } catch (error) {
-      req.flash('errors', 'We cannot get to New Category page at the moment please try again and contact developer');
+      req.flash('errors', 'We cannot get to Edit Category page at the moment please try again and contact developer');
       res.redirect(301, '/api/users/projects/all-projects');
     }   
   }
