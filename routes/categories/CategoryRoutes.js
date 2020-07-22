03558482fb9544ab38ae63ab4ee4ee8afdf74d8e
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-07-22 12:40:42 -0400

    before lunch commit

--- routes/categories/CategoryRoutes.js
+++ routes/categories/CategoryRoutes.js
@@ -5,7 +5,7 @@ const { isThereAuth } = require('../utils/isThereAuth');
 router.get('/new-category', isThereAuth, newCategoryGET);
 router.post('/new-category', isThereAuth, newCategoryPOST);
 
-// router.get('/edit-category', isThereAuth, editCategoryGET);
-// router.post('/edit-category/:categoryId', isThereAuth, editCategoryPOST);
+router.get('/edit-category', isThereAuth, editCategoryGET);
+router.put('/edit-category/:categoryId', isThereAuth, editCategoryPOST);
 
 module.exports = router
